590c3853f5e62085fd8e21544620c61deb348f01
Shiyas Ahamed S <shiyas.s@experionglobal.local>
2024-01-15 14:49:19 +0530

    feat added ReportTypeModel

--- mentorship_program_tool/Data/AppDbContext.cs
+++ mentorship_program_tool/Data/AppDbContext.cs
@@ -12,39 +12,7 @@ namespace mentorship_program_tool.Data
         public DbSet<EmployeeModel> Employees { get; set; }
 
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Your model configurations here...
-
-            SeedData(modelBuilder);
-
-            base.OnModelCreating(modelBuilder);
-        }
-        private static void SeedData(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<City>().HasData(
-                new City { CityId = 1, CityName = "Cochin" },
-                new City { CityId = 2, CityName = "Trivandrum" }
-                // Add more cities as needed...
-            );
-
-            modelBuilder.Entity<Designation>().HasData(
-                new Designation { DesignationId = 1, DesignationName = "Manager" },
-                new Designation { DesignationId = 2, DesignationName = "Developer" }
-                // Add more designations as needed...
-            );
-
-            modelBuilder.Entity<Education>().HasData(
-                new Education { EducationId = 1, EducationLevel = "Bachelor's Degree" },
-                new Education { EducationId = 2, EducationLevel = "Master's Degree" }
-                // Add more education levels as needed...
-            );
-
-            modelBuilder.Entity<Department>().HasData(
-                new Department { DepartmentId = 1, DepartmentName = "IT" },
-                new Department { DepartmentId = 2, DepartmentName = "HR" }
-                // Add more departments as needed...
-            );
-        }
+       
+        
     }
 }
---
+++ mentorship_program_tool/Models/EntityModel/ReportTypeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace mentorship_program_tool.Models.EntityModel
+{
+    public class ReportTypeModel
+    {
+        public int reporttypeid { get; set; }
+        public string reporttype { get; set; }
+    }
+}
