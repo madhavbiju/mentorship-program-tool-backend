3b0c57ae6e9d3f67317b3e4735722d8709d097cd
Shiyas Ahamed S <shiyas.s@experionglobal.com>
2024-01-23 09:39:55 +0530

    feat: Get Mentee details for Mentee Page Done

--- mentorship_program_tool/UnitOfWork/UnitOfWork.cs
+++ mentorship_program_tool/UnitOfWork/UnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿using mentorship_program_tool.Data;
 using mentorship_program_tool.Repository;
+using mentorship_program_tool.Repository.GetAllMenteeDetailsByIdRepository;
 
 namespace mentorship_program_tool.UnitOfWork
 {
@@ -23,6 +24,8 @@ namespace mentorship_program_tool.UnitOfWork
 
             getAllProgramsRepository = new GetAllProgramsRepository(_context);
 
+            getMenteeDetailsByIdRepository = new GetMenteeDetailsByIdRepository(_context);
+
             // Initialize other repositories.
         }
 
@@ -36,7 +39,7 @@ namespace mentorship_program_tool.UnitOfWork
         public IAdminApprovalRequestRepository adminApprovalRequestRepository { get; }
         public IGetAllProgramsRepository getAllProgramsRepository { get; }
 
-
+        public IGetMenteeDetailsByIdRepository getMenteeDetailsByIdRepository { get; }
 
         public int Complete()
         {
