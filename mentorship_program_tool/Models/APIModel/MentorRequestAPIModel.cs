e220e74a5dcd20c70b8038761811eb134dfdea29
Madhav Biju <65170463+madhavbiju@users.noreply.github.com>
2024-01-24 09:28:52 +0530

    fix: Requested Mentor cannot be Null

--- mentorship_program_tool/Models/APIModel/MentorRequestAPIModel.cs
+++ mentorship_program_tool/Models/APIModel/MentorRequestAPIModel.cs
@@ -9,6 +9,7 @@ namespace mentorship_program_tool.Models.APIModel
         public int programid { get; set; }
         public DateTime newenddate { get; set; }
         public string reason { get; set; }
+        public int modifiedby { get; set; }
         public int RequestStatusId { get; set; } = 2;
 
 
