905acb2a2643949163252cb207fb37a07f8edfbb
Madhav Biju <65170463+madhavbiju@users.noreply.github.com>
2024-01-26 20:11:09 +0530

    feat: Add API for creating new program

---
+++ mentorship_program_tool/Controllers/ProgramController.cs
@@ -0,0 +1,58 @@
+﻿using mentorship_program_tool.Models.EntityModel;
+using mentorship_program_tool.Services.ProgramService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mentorship_program_tool.Controllers
+{
+    
+
+    [ApiController]
+    [Route("api/[controller]")]
+public class ProgramController : ControllerBase
+    {
+        private readonly IProgramService _programService;
+
+        public ProgramController(IProgramService programService)
+        {
+            _programService = programService;
+        }
+
+        [HttpGet]
+        public IActionResult GetPrograms()
+        {
+            var program = _programService.GetProgram();
+            return Ok(program);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProgramsById(int id)
+        {
+            var program = _programService.GetProgramById(id);
+            if (program == null)
+            {
+                return NotFound();
+            }
+            return Ok(program);
+        }
+
+        [HttpPost]
+        public IActionResult AddPrograms(ProgramModel program)
+        {
+            _programService.CreateProgram(program);
+            return CreatedAtAction(nameof(GetProgramsById), new { id = program.programid }, program);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePrograms(int id)
+        {
+            if (id != null)
+            {
+                _programService.DeleteProgram(id);
+                return NoContent();
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ mentorship_program_tool/Services/ProgramService/IProgramService.cs
@@ -0,0 +1,12 @@
+﻿using mentorship_program_tool.Models.EntityModel;
+
+namespace mentorship_program_tool.Services.ProgramService
+{
+    public interface IProgramService
+    {
+        IEnumerable<ProgramModel> GetProgram();
+        ProgramModel GetProgramById(int id);
+        void CreateProgram(ProgramModel program);
+        void DeleteProgram(int id);
+    }
+}
