e0c2ade7ab6555d68c1f3ec722f7b2a8982304a3
Aadarsh R <aadarsh.r@experionglobal.com>
2024-02-06 13:40:12 +0530

    Added Listing  All Users functionality

--- mentorship_program_tool/Services/GetUserDetailsService/GetUserDetailsService.cs
+++ mentorship_program_tool/Services/GetUserDetailsService/GetUserDetailsService.cs
@@ -1,5 +1,7 @@
 ﻿using mentorship_program_tool.Data;
 using mentorship_program_tool.Models.APIModel;
+using mentorship_program_tool.Models.EntityModel;
+using System.Linq;
 
 namespace mentorship_program_tool.Services.GetUserDetailsService
 {
@@ -14,39 +16,47 @@ namespace mentorship_program_tool.Services.GetUserDetailsService
 
         public UserDetailsResponseAPIModel GetUserDetails(string role, int pageNumber, int pageSize)
         {
-            var roleId = _context.Roles.FirstOrDefault(r => r.RoleName == role)?.RoleID;
-
-            if (roleId == null)
+            IQueryable<EmployeeRoleMapping> roleMappingsQuery;
+            int? roleId = null;
+            if (role.ToLower() != "all")
             {
-                return new UserDetailsResponseAPIModel { Users = Enumerable.Empty<GetUserDetailsAPIModel>(), TotalCount = 0 };
+                roleId = _context.Roles.FirstOrDefault(r => r.RoleName == role)?.RoleID;
+                if (roleId == null)
+                {
+                    return new UserDetailsResponseAPIModel { Users = Enumerable.Empty<GetUserDetailsAPIModel>(), TotalCount = 0 };
+                }
+                roleMappingsQuery = _context.EmployeeRoleMappings.Where(erm => erm.RoleID == roleId);
+            }
+            else
+            {
+                roleMappingsQuery = _context.EmployeeRoleMappings;
             }
 
             // First, get the total count of users with the specified role (before applying pagination)
-            var totalCount = _context.Employees
-                .Join(_context.EmployeeRoleMappings, e => e.EmployeeID, erm => erm.EmployeeID, (e, erm) => erm)
-                .Count(erm => erm.RoleID == roleId);
+            var totalCount = roleMappingsQuery
+                .Join(_context.Employees, erm => erm.EmployeeID, e => e.EmployeeID, (erm, e) => e)
+                .Count();
 
             // Calculate the number of records to skip for pagination
             int skip = (pageNumber - 1) * pageSize;
 
-            var userList = _context.Employees
-                .Join(_context.EmployeeRoleMappings, e => e.EmployeeID, erm => erm.EmployeeID, (e, erm) => new { Employee = e, EmployeeRoleMapping = erm })
-                .Where(x => x.EmployeeRoleMapping.RoleID == roleId)
+            var userListQuery = roleMappingsQuery
+                .Join(_context.Employees, erm => erm.EmployeeID, e => e.EmployeeID, (erm, e) => new { Employee = e, EmployeeRoleMapping = erm })
                 .Select(x => new GetUserDetailsAPIModel
                 {
                     UserID = x.Employee.EmployeeID,
                     UserName = x.Employee.FirstName + " " + x.Employee.LastName,
-                    UserRole = role,
-                    UserJob = "Lead Tester",
+                    UserRole = roleId.HasValue ? role : _context.Roles.FirstOrDefault(r => r.RoleID == x.EmployeeRoleMapping.RoleID).RoleName,
+                    UserJob = "Lead Tester", // Assuming static, adjust as necessary
                     UserStatus = x.Employee.AccountStatus
-                })
+                });
+
+            var userList = userListQuery
                 .Skip(skip)
                 .Take(pageSize)
                 .ToList();
 
             return new UserDetailsResponseAPIModel { Users = userList, TotalCount = totalCount };
         }
-
-
     }
 }
