cfa4f9f6d6b91291f7875dc24a7f8df80dae3319
Shiyas Ahamed S <shiyas.s@experionglobal.com>
2024-02-06 12:02:26 +0530

    feat\Cors updated

--- mentorship_program_tool/Program.cs
+++ mentorship_program_tool/Program.cs
@@ -118,7 +118,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins("http://localhost:5173")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
