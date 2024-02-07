c2340d61d2ab6f48957189f89984c080d4679006
Aadarsh R <aadarsh.r@experionglobal.com>
2024-02-07 12:56:23 +0530

    Corrected CORS

--- mentorship_program_tool/Program.cs
+++ mentorship_program_tool/Program.cs
@@ -126,7 +126,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")
+            policy.WithOrigins("http://localhost:3000")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
