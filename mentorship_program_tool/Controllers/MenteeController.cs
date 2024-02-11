363fc777c53524fd078f73e0390420a5e9546f1e
Mehanoor Basheer <mehanoorbasheer7@gmail.com>
2024-02-11 10:52:23 +0530

    feat:sort and filter for menteeslist of mentor

--- mentorship_program_tool/Controllers/MenteeController.cs
+++ mentorship_program_tool/Controllers/MenteeController.cs
@@ -30,14 +30,14 @@ namespace mentorship_program_tool.Controllers
         /// To get all mentees under a mentor.
         /// </summary>
         [HttpGet("mentor/{id}")]
-        public ActionResult<GetAllMenteesOfMentorResponseAPIModel> GetAllMenteesById(int id, [FromQuery] int pageNumber, [FromQuery] int pageSize)
+        public ActionResult<GetAllMenteesOfMentorResponseAPIModel> GetAllMenteesById(int id, [FromQuery] int pageNumber, [FromQuery] int pageSize, string? sortBy, string? filterBy, DateTime? endDateFilter)
         {
             if (pageNumber <= 0 || pageSize <= 0)
             {
                 return BadRequest("PageNumber and PageSize must be greater than 0.");
             }
 
-            var menteesList = _getAllMenteesOfMentorService.GetAllMenteesById(id, pageNumber, pageSize);
+            var menteesList = _getAllMenteesOfMentorService.GetAllMenteesById(id, pageNumber, pageSize, sortBy, filterBy, endDateFilter);
 
             if (menteesList == null)
             {
