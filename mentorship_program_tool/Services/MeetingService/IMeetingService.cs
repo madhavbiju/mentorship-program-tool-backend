702dc23b1df769c8d1c3e15b507105d0f862b6e1
Shiyas Ahamed S <shiyas.s@experionglobal.com>
2024-02-13 17:34:58 +0530

    fix\added new api to get the meetings

--- mentorship_program_tool/Services/MeetingService/IMeetingService.cs
+++ mentorship_program_tool/Services/MeetingService/IMeetingService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using mentorship_program_tool.Models.ApiModel;
 using mentorship_program_tool.Models.EntityModel;
 
 namespace mentorship_program_tool.Services.MeetingService
@@ -6,6 +7,8 @@ namespace mentorship_program_tool.Services.MeetingService
     public interface IMeetingService
     {
         IEnumerable<MeetingSchedule> GetMeetings();
+        GetAllMeetingsResponseAPIModel GetAllMeetings(int pageNumber, string sort);
+
         IEnumerable<MeetingSchedule> GetMeetingByEmployeeId(int id, int role);
         IEnumerable<MeetingSchedule> GetSoonMeetingByEmployeeId(int id);
         MeetingSchedule GetMeetingById(int id);
--- mentorship_program_tool/Services/MeetingService/MeetingService.cs
+++ mentorship_program_tool/Services/MeetingService/MeetingService.cs
@@ -1,9 +1,12 @@
 ﻿using mentorship_program_tool.Data;
+using mentorship_program_tool.Models.ApiModel;
+using mentorship_program_tool.Models.APIModel;
 using mentorship_program_tool.Models.EntityModel;
 using mentorship_program_tool.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
 
 namespace mentorship_program_tool.Services.MeetingService
@@ -24,6 +27,51 @@ namespace mentorship_program_tool.Services.MeetingService
         {
             return _unitOfWork.MeetingSchedule.GetAll();
         }
+
+        public GetAllMeetingsResponseAPIModel GetAllMeetings(int pageNumber, string sortBy)
+        {
+            int pageSize = 5; // Assuming 5 records per page
+
+            IQueryable<GetAllMeetingsAPIModel> query = (from ms in _context.MeetingSchedules
+                                                        join p in _context.Programs on ms.ProgramID equals p.ProgramID
+                                                        join mentor in _context.Employees on p.MentorID equals mentor.EmployeeID
+                                                        join mentee in _context.Employees on p.MenteeID equals mentee.EmployeeID
+                                                        join status in _context.Statuses on ms.MeetingStatus equals status.StatusID
+                                                        select new GetAllMeetingsAPIModel
+                                                        {
+                                                            meetingID = ms.MeetingID, // Include meetingID
+                                                            menteeName = mentee.FirstName + " " + mentee.LastName,
+                                                            scheduledDate = ms.ScheduleDate,
+                                                            scheduledTime = ms.StartTime,
+                                                            meetingName = ms.Title,
+                                                            Status = status.StatusValue
+                                                        });
+
+            // Apply sorting based on the sortBy parameter
+            switch (sortBy)
+            {
+                case "date":
+                    query = query.OrderBy(ms => ms.scheduledDate);
+                    break;
+                case "menteeName":
+                    query = query.OrderBy(ms => ms.menteeName);
+                    break;
+                default:
+                    // Default sorting by date if sortBy parameter is not recognized
+                    query = query.OrderByDescending(ms => ms.scheduledDate);
+                    break;
+            }
+            int totalCount = query.Count();
+
+            // Apply pagination
+            query = query.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+
+            return new GetAllMeetingsResponseAPIModel { Meetings = query.ToList(), TotalCount = totalCount };
+        }
+
+
+
+
 
         public IEnumerable<MeetingSchedule> GetMeetingByEmployeeId(int id, int role)
         {
